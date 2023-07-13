ac9d82875e525af98adb4134fb992198ae2f0756
Gavin Morrow <gavinfmorrow@gmail.com>
2023-07-13 12:48:18 -0400

    Initial Commit

---
+++ GavCam/GavCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GavCamApp.swift
+//  GavCam
+//
+//  Created by Gavin Morrow on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct GavCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
